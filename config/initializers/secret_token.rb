22a48842ebe132b6d4846a2e0bd3eb1ec4bb63b2
Eli Gundry <eligundry@gmail.com>
2012-09-11 18:09:33 -0400

    Inital commit with Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carfox::Application.config.secret_token = '2789e3f35c950035473340a591c55c88973f6515e9e8a976dcddaf2a78230d9ed3eb9de457e3adc488a9c6c576d2e8307237d44adb8a508117d99687193a8144'
